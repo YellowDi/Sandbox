a657c4c38ac0cd938ee9fdb1247beb0045fda2df
YellowDi <yellowdi@me.com>
2014-07-07 16:51:09 +0800

    版本号变动

--- app/config/version.php
+++ app/config/version.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array (
-  'sys_name' => 'StartBBS',
-  'sys_url' => 'http://www.startbbs.com',
+  'sys_name' => 'SimCity.Pro',
+  'sys_url' => 'http://simcity.pro',
   'sys_version' => 'V1.1.5',
-  'sys_update' => '20140306',
-  'sys_author' => 'QQ858292510',
+  'sys_update' => '20140704',
+  'sys_author' => 'YellowDi',
 );
