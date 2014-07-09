70617fbae4fb86b0e8a2dea40f8fabfb19248400
YellowDi <yellowdi@me.com>
2014-07-09 22:38:55 +0800

    增加版本号

--- app/config/version.php
+++ app/config/version.php
@@ -3,7 +3,7 @@
 $config = array (
   'sys_name' => 'SimCity.Pro',
   'sys_url' => 'http://simcity.pro',
-  'sys_version' => 'V1.1.5',
-  'sys_update' => '20140704',
+  'sys_version' => 'V1.1.5。3',
+  'sys_update' => '20140709',
   'sys_author' => 'YellowDi',
 );
